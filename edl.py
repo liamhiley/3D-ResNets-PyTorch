1b74872ecec3a624938a41d404cbf01856bc654e
Marc Roig Vilamala <marc.roig.vilamala@gmail.com>
2019-04-04 17:01:13 +0100

    Added KL function

--- edl.py
+++ edl.py
@@ -3,6 +3,22 @@ import torch
 def relu_evidence(logits):
     return torch.nn.ReLU(logits)
 
+
+def KL(alpha):
+    beta = torch.from_numpy(np.ones((1, alpha.shape()[1])))
+    S_alpha = alpha.sum(axis=1, keep_dims=True)
+    S_beta = beta.sum(axis=1, keep_dims=True)
+    lnB = torch.lgamma(S_alpha, axis=1, keep_dims=True)
+    lnB_uni = torch.lgamma(beta, axis=1, keep_dims=True).sum(axis=1, keep_dims=True) - torch.lgamma(S_beta, axis=1, keep_dims=True)
+
+    dg0 = torch.digamma(S_alpha)
+    dg1 = torch.digamma(alpha)
+
+    kl = ((alpha - beta) * (dg1 - dg0)).sum(axis=1, keep_dims=True) + lnB + lnB_uni
+
+    return kl
+
+
 def mse_loss(p, alpha, global_step, annealing_step):
     S = alpha.sum(dim=1, keep_dims=True)
     E = alpha - 1
